7de3d8716e3fba8d0697be85d1f4cd41139e565f
Desmond Sow <chun.jieh.sow@intel.com>
2022-04-25 14:36:54 +0800

    Migrate existing database `Seeders` files

--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Device;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class DeviceSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            DeviceStatusSeeder::class,
-        ]);
+        Device::factory(10)->create();
     }
 }
