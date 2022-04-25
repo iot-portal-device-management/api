1d585c8832ac162370c669858d17433d30b6e796
Desmond Sow <chun.jieh.sow@intel.com>
2022-04-25 14:18:30 +0800

    Migrate existing `Requests` files

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponses;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseFormRequest extends FormRequest
+{
+    use ApiResponses;
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException($this->apiBadRequest(errors: $validator->getMessageBag()->toArray()));
+    }
+}
