b333214e3cc06d67f5f5f75953e866f28f118ad4
Desmond Sow <chun.jieh.sow@intel.com>
2022-04-25 13:45:27 +0800

    Migrate existing `Traits`

---
+++ app/Traits/HasDeviceConnectionKey.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Crypt;
+
+trait HasDeviceConnectionKey
+{
+    public static function deviceConnectionKeyExists($value)
+    {
+        return static::where('device_connection_key', $value)->exists();
+    }
+
+    public static function generateEncryptedDeviceConnectionKey(): string
+    {
+        $deviceConnectionKey = Crypt::encryptString(bin2hex(random_bytes(32)));
+
+        if (static::deviceConnectionKeyExists($deviceConnectionKey)) {
+            return static::generateEncryptedDeviceConnectionKey();
+        }
+
+        return $deviceConnectionKey;
+    }
+
+    public static function decryptDeviceConnectionKey($value): string
+    {
+        return Crypt::decryptString($value);
+    }
+
+    public function validateDeviceConnectionKey($value): bool
+    {
+        return $this->device_connection_key === $value;
+    }
+
+    public function getDeviceConnectionKeyAttribute($value): string
+    {
+        return static::decryptDeviceConnectionKey($value);
+    }
+}
